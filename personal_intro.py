b6b466b8e11bc18294b98b74ba5e7dd741371012
donvincent195 <111957783+donvincent195@users.noreply.github.com>
2026-02-12 18:05:13 +0400

    1st Task introduction

---
+++ personal_intro.py
@@ -0,0 +1,14 @@
+#Personal Info
+
+#Ask the user for their name,age and hobby, and store it in the variables
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+
+#Print a welcome message with the user's name
+print("\n🎉 Welcome " + name + "! 🎉")
+
+#f is called an f-string (formatted string).
+print(f"You are {age} years old and love {hobby}.")
+
